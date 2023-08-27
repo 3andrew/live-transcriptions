33da8600539f93b7d15210d8ddbdfdd1ae95a20c
Andrew <andrew123709@gmail.com>
2023-08-27 18:29:11 -0400

    add api call

--- popup.js
+++ popup.js
@@ -1,54 +1,73 @@
 function record() {
-    chrome.tabs.query(
+
+    // get the audio stream
+    chrome.tabCapture.capture(
         {
-            active: true,
-            currentWindow: true,
+            audio: true,
         },
-        (tabs) => {
-            const tabId = tabs[0].id;
-            // get the stream
-            chrome.tabCapture.capture(
-                {
-                    audio: true,
-                    video: false
-                },
-                (stream) => {
-                    console.log("stream:", stream);
+        (stream) => {
+            console.log("stream:", stream);
 
-                    // continue projecting audio to user
-                    const output = new AudioContext();
-                    const source = output.createMediaStreamSource(stream);
-                    source.connect(output.destination);
+            // continue projecting audio to user
+            const output = new AudioContext();
+            const source = output.createMediaStreamSource(stream);
+            source.connect(output.destination);
 
-                    // create mediarecorder to get blob from mediastream
-                    let recordedData = [];
-                    const mediaRecorder = new MediaRecorder(stream, {mimeType: "audio/webm"});
+            // create mediarecorder to get blob from mediastream
+            const mediaRecorder = new MediaRecorder(stream, {mimeType: "audio/webm"});
 
-                    mediaRecorder.start();
+            mediaRecorder.start();
 
-                    // stop mediarecorder when button pressed
-                    document.getElementById( 'stop-record-button' ).addEventListener('click', () => {
-                        mediaRecorder.stop();
-                    } );
+            // stop mediarecorder when button pressed
+            document.getElementById( "stop-record-button").addEventListener("click", () => {
+                mediaRecorder.stop();
+            } );
 
-                    // convert to file when recording ends
-                    mediaRecorder.onstop = (event) => {
-                        recordedData.push(event.data)
-                        console.log("data:", recordedData);
+            // convert to file when recording ends
+            mediaRecorder.ondataavailable = (event) => {
+                // to do: fix undefined event here
+                console.log("data:", event.data);
 
-                        const blob = new Blob(recordedData , {type: "audio/webm"});
-                        const file = new File( [ blob ], "file.webm", {type: "audio/webm"} );
-                        console.log("file:", file);
-                    }
+                const blob = new Blob(event.data, {type: "audio/webm"});
+                const file = new File( [ blob ], "file.webm", {type: "audio/webm"} );
+                console.log("file:", file);
 
+                // convert to body of request
+                const formData = new FormData();
+                console.log("test1");
+                formData.append("model", "whisper-1");
+                formData.append("file", file);
+                console.log("request body:", formData);
 
-
-                }
-            );
+                const text = getTranscription(formData);
+                console.log("transcription:", text);
+            }
         }
     );
 }
-document.getElementById( 'record-button' ).addEventListener('click', () => {
+document.getElementById( "record-button" ).addEventListener("click", () => {
     record();
 } );
 
+// parameters: body: model, file, language
+// returns string
+const getTranscription = async (data) => {
+
+    try {
+        // make request to openai api https://platform.openai.com/docs/api-reference/audio/createTranscription
+        const response = await fetch("https://api.openai.com/v1/audio/transcriptions", {
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer $API_TOKEN",
+                "Content-Type": "multipart/form-data"
+            },
+            body: data
+        });
+        const transcription = await response.json();
+        return transcription.text;
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
+
+}
